b050aaeafbe332fdbd59597fb35e98c99330fc32
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2025-07-29 00:20:54 -0300

    feat: add qrcode.react library and integrate QR code generation in DialogDownloadPubkyRing component

--- src/components/ui/molecules/DialogDownloadPubkyRing/DialogDownloadPubkyRing.tsx
+++ src/components/ui/molecules/DialogDownloadPubkyRing/DialogDownloadPubkyRing.tsx
@@ -1,5 +1,6 @@
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui';
 import Image from 'next/image';
+import { QRCodeSVG } from 'qrcode.react';
 
 export function DialogDownloadPubkyRing({ store = 'apple' }: { store?: 'apple' | 'android' }) {
   const imageBadge =
@@ -9,14 +10,14 @@ export function DialogDownloadPubkyRing({ store = 'apple' }: { store?: 'apple' |
           alt: 'App Store',
           width: 120,
           height: 40,
-          href: 'https://apps.apple.com/us/app/pubky-ring/id6749690000',
+          href: 'https://apps.apple.com/mx/app/pubky-ring/id6739356756',
         }
       : {
           src: '/images/badge-android.png',
           alt: 'Google Play',
           width: 120,
           height: 40,
-          href: 'https://play.google.com/store/apps/details?id=com.pubky.ring',
+          href: 'https://play.google.com/store/apps/details?id=to.pubky.ring&pcampaignid=web_share',
         };
 
   return (
@@ -48,9 +49,11 @@ export function DialogDownloadPubkyRing({ store = 'apple' }: { store?: 'apple' |
           <div className="flex gap-8 items-center">
             <Image src="/images/pubky-ring-phone.png" alt="App preview" width={96} height={256} />
             <div className="flex flex-col space-y-6">
-              <div className="bg-foreground rounded-lg p-4 w-[192px] h-[192px]">QRCODE</div>
+              <div className="bg-foreground rounded-lg p-4 w-[192px] h-[192px] flex items-center justify-center">
+                <QRCodeSVG value={imageBadge.href} size={160} />
+              </div>
               <div className="flex justify-between items-center w-full">
-                <a className="h-10 w-full" href="https://www.pubkyring.to/" target="_blank">
+                <a className="h-10 w-full" href={imageBadge.href} target="_blank">
                   <Image
                     src={imageBadge.src}
                     alt={imageBadge.alt}
