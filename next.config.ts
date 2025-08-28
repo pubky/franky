77fa4c9f82f146e8b9c6eaf42b3a523a818218ae
tipogi <tipogi@protonmail.com>
2025-08-28 17:07:46 +0200

    update pubky-app-specs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['pubky-app-specs'],
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals.push('@synonymdev/pubky');
