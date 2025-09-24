fefbf65ebb09ce50946d87330a284d63f6034f47
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2025-09-24 20:16:52 -0300

    refactor: remove Badge examples and types, update Badge component to render as div by default

--- src/components/atoms/Badge/Badge.types.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export type BadgeVariant = 'default' | 'secondary' | 'destructive' | 'outline';
-
-export interface BadgeProps extends React.ComponentProps<'span'> {
-  className?: React.HTMLAttributes<HTMLSpanElement>['className'];
-  variant?: BadgeVariant;
-  asChild?: boolean;
-}
