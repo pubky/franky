58aef895310a0abb0ce239b21d56cd240a9e9a0e
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2025-09-30 11:47:51 +0200

    chore: restore onboarding/profile/page.tsx

---
+++ src/app/onboarding/profile/page.tsx
@@ -0,0 +1,11 @@
+import * as Templates from '@/templates';
+import * as Molecules from '@/molecules';
+
+export const metadata = Molecules.Metadata({
+  title: 'Profile - Onboarding',
+  description: 'Onboarding profile page on pubky app.',
+});
+
+export default function ProfilePage() {
+  return <Templates.Profile />;
+}
