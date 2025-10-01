7ec7a029ff221f02b08cf4d689a80932fd1e7dba
Flavio Moceri <moceri.flavio@gmail.com>
2025-10-01 11:01:22 +0200

    fix popovertradeoffs

--- src/components/molecules/PopoverTradeoffs/PopoverTradeoffs.tsx
+++ src/components/molecules/PopoverTradeoffs/PopoverTradeoffs.tsx
@@ -9,7 +9,7 @@ export function PopoverTradeoffs({ ...props }: React.HTMLAttributes<HTMLDivEleme
           <Libs.AlertTriangle className="h-4 w-4" data-testid="alert-triangle-icon" />
         </Atoms.Button>
       </Atoms.PopoverTrigger>
-      <Atoms.PopoverContent className="w-full">
+      <Atoms.PopoverContent>
         <Atoms.Container className="gap-2 px-3 py-2">
           <Atoms.Container className="gap-2">
             <Atoms.Heading level={4} size="sm" className="text-popover-foreground">
