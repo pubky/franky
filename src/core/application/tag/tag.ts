d4b8974810052e1eda4271152cc04cc5bff29536
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2025-10-03 16:00:02 +0200

    fix: convert to static class for application

--- src/core/application/tag/tag.ts
+++ src/core/application/tag/tag.ts
@@ -1,17 +1,14 @@
 import * as Core from '@/core';
 import type { TCreateTagInput, TDeleteTagInput } from './tag.types';
 
-async function create({ postId, label, taggerId, tagUrl, tagJson }: TCreateTagInput) {
-  await Core.Local.Tag.save({ postId, label, taggerId });
-  await Core.HomeserverService.request(Core.HomeserverAction.PUT, tagUrl, tagJson);
-}
+export class Tag {
+  static async create({ postId, label, taggerId, tagUrl, tagJson }: TCreateTagInput) {
+    await Core.Local.Tag.save({ postId, label, taggerId });
+    await Core.HomeserverService.request(Core.HomeserverAction.PUT, tagUrl, tagJson);
+  }
 
-async function deleteTag({ postId, label, taggerId, tagUrl }: TDeleteTagInput) {
-  await Core.Local.Tag.remove({ postId, label, taggerId });
-  await Core.HomeserverService.request(Core.HomeserverAction.DELETE, tagUrl);
+  static async delete({ postId, label, taggerId, tagUrl }: TDeleteTagInput) {
+    await Core.Local.Tag.remove({ postId, label, taggerId });
+    await Core.HomeserverService.request(Core.HomeserverAction.DELETE, tagUrl);
+  }
 }
-
-export const Tag = {
-  create,
-  delete: deleteTag,
-};
