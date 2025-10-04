427f2325ef8e5094a65a8ea639e3d453b9b63822
James Browning <jamesbrowning91@gmail.com>
2025-10-04 12:07:20 +0100

    tests: fix e2e onboarding with new welcome screen and backup reminder

--- cypress/e2e/onboarding.cy.ts
+++ cypress/e2e/onboarding.cy.ts
@@ -19,6 +19,19 @@ describe('Onboarding', () => {
     const backupWithFileAndPhrase = [BackupType.EncryptedFile, BackupType.RecoveryPhraseWithConfirmation];
     cy.onboardAsNewUser(profileName, 'Test Bio', backupWithFileAndPhrase);
 
+    // confirm backup reminder is shown
+    cy.get('#backup-btn').should('exist').click();
+    // confirm backup dialog can be shown and closed
+    cy.get('#backup-dialog-title').should('exist');
+    cy.get('#dialog-close-btn').click();
+    // confirm backup done button can be clicked
+    cy.get('#backup-done-btn').click();
+    // confirm backup confirmation dialog can be shown and dismissed
+    cy.get('#backup-done-warning-text').should('exist');
+    cy.get('#backup-done-confirm-btn').click();
+    // confirm backup reminder is not shown
+    cy.get('#backup-btn').should('not.exist');
+
     cy.signOut(HasBackedUp.Yes);
 
     cy.signInWithEncryptedFile(backupDownloadFilePath(profileName));
