15726a0178ab48a7a841bbaa32a118bc9cc098c4
tipogi <tipogi@protonmail.com>
2025-10-04 10:59:22 +0200

    rename stream table to post_stream

--- src/core/models/stream/post/postStream.test.ts
+++ src/core/models/stream/post/postStream.test.ts
@@ -1,8 +1,13 @@
 import { db } from '@/core/database';
 import { describe, it, expect, beforeEach } from 'vitest';
+<<<<<<< HEAD
 import { createDefaultPostStream } from './postStream.helper';
 import { PostStreamModel } from './postStream';
 import { PostStreamTypes } from './postStream.types';
+=======
+import { createDefaultStream } from './postStream.helper';
+import { PostStreamModel } from './postStream';
+>>>>>>> 8f93f84 (rename stream table to post_stream)
 
 describe('PostStreamModel', () => {
   beforeEach(async () => {
