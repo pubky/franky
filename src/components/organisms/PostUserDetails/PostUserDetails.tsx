2b57de82de6f7e4b582809fed31db919145e4900
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2025-10-06 14:59:43 -0300

    refactor: reuse formatPublicKey function

--- src/components/organisms/PostUserDetails/PostUserDetails.tsx
+++ src/components/organisms/PostUserDetails/PostUserDetails.tsx
@@ -31,7 +31,7 @@ export function PostUserDetails({ postId }: PostUserDetailsProps) {
           </Atoms.Typography>
           <Atoms.Container className="flex flex-row gap-2">
             <Atoms.Typography size="sm" className="text-muted-foreground uppercase">
-              {Libs.formatPubky(userDetails.id)}
+              {Libs.formatPublicKey({ key: userDetails.id, length: 4 })}
             </Atoms.Typography>
             <Atoms.Container className="flex flex-row gap-1 items-center">
               <Libs.Clock className="h-4 w-4 text-muted-foreground" />
