119ef06ac020af383d3da2e63e061bb3bacd5d3c
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2025-10-08 23:27:56 -0300

    refactor: update routing and component structure for landing page
    - Changed navigation from '/feed' to '/home' in Cypress commands.
    - Renamed Home component to LandingPage and updated its template.
    - Introduced new Landing template with structured layout components.

---
+++ src/components/templates/Landing/Landing.tsx
@@ -0,0 +1,18 @@
+import * as Atoms from '@/atoms';
+import * as Molecules from '@/molecules';
+
+export function Landing() {
+  return (
+    <>
+      <Atoms.ImageBackground image="/images/bg-home.svg" mobileImage="/images/bg-home-mobile.svg" />
+      <Atoms.Container size="container" className="px-6">
+        <Molecules.PageContainer size="narrow" className="items-start mx-0 flex flex-col gap-6">
+          <Molecules.HomePageHeading />
+          <Molecules.HomeSectionTitle />
+          <Molecules.HomeActions />
+          <Molecules.HomeFooter />
+        </Molecules.PageContainer>
+      </Atoms.Container>
+    </>
+  );
+}
