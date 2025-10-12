44efa212d353ec4de2b8c2b49a9179d8a823d291
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2025-10-12 01:11:54 -0300

    chore: remove HomeLoader component to streamline the codebase

--- src/app/home/loader.tsx
+++
@@ -1,9 +0,0 @@
-import * as Libs from '@/libs';
-
-export default function HomeLoader() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Libs.Loader2 className="h-8 w-8 animate-spin text-primary" />
-    </div>
-  );
-}
