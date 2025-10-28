c0a8b8bff4b2d01287388f2d9f72f83b1fff65bf
V <jovanovicv90@gmail.com>
2025-10-28 13:16:16 +0100

    refactor: use index.ts for DialogBackupPhrase component export
    - Create index.ts file in DialogBackupPhrase directory following component pattern
    - Update organisms index to use DialogBackupPhrase index instead of direct file path
    - Maintains consistency with other component exports
    - All tests passing and build successful

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -4,7 +4,7 @@ export * from './ContentLayout';
 export * from './CreateProfileHeader';
 export * from './CreateProfileForm';
 export * from './DialogBackup';
-export * from './DialogBackupPhrase/DialogBackupPhrase';
+export * from './DialogBackupPhrase';
 export * from './DialogConfirmBackup';
 export * from './DialogBackupEncrypted';
 export * from './DialogRestoreEncryptedFile';
