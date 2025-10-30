8bf42e0a0aaee61af5b49821b6ddeac854420071
SHAcollision <127778313+SHAcollision@users.noreply.github.com>
2025-10-30 22:10:21 +0100

    chore: add cdn url and remove universal API version config (#280)

--- src/config/nexus.ts
+++ src/config/nexus.ts
@@ -1,5 +1,5 @@
 import { Env } from '@/libs';
 
 export const NEXUS_URL = Env.NEXT_PUBLIC_NEXUS_URL;
-export const NEXUS_VERSION = Env.NEXT_PUBLIC_NEXUS_VERSION;
 export const NEXUS_NOTIFICATIONS_LIMIT = 30;
+export const CDN_URL = Env.NEXT_PUBLIC_CDN_URL;
