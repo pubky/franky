36b651983f6039a21133f85f4d88289e68bb5b28
tipogi <tipogi@protonmail.com>
2025-10-31 06:42:17 +0100

    make synchronous the normalizer functions

--- src/core/pipes/notification/notification.normalizer.ts
+++ src/core/pipes/notification/notification.normalizer.ts
@@ -1,11 +1,14 @@
 import { LastReadResult } from 'pubky-app-specs';
 import * as Core from '@/core';
+import * as Libs from '@/libs';
 
 export class NotificationNormalizer {
   private constructor() {}
 
   static to(pubky: Core.Pubky): LastReadResult {
     const builder = Core.PubkySpecsSingleton.get(pubky);
-    return builder.createLastRead();
+    const result = builder.createLastRead();
+    Libs.Logger.debug('LastRead validated', { result });
+    return result;
   }
 }
