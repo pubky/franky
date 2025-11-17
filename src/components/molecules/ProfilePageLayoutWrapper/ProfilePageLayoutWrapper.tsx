1e129698809adde12d4bf21f69a1ec0bed78b583
V <jovanovicv90@gmail.com>
2025-11-17 13:38:13 +0100

    chore(profile): replace span with Typography in ProfilePageLinks and fix Container defaults

--- src/components/molecules/ProfilePageLayoutWrapper/ProfilePageLayoutWrapper.tsx
+++ src/components/molecules/ProfilePageLayoutWrapper/ProfilePageLayoutWrapper.tsx
@@ -6,7 +6,10 @@ export interface ProfilePageLayoutWrapperProps {
 
 export function ProfilePageLayoutWrapper({ children }: ProfilePageLayoutWrapperProps) {
   return (
-    <Atoms.Container className="max-w-sm px-6 pt-0 pb-12 sm:max-w-xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl xl:px-0">
+    <Atoms.Container
+      overrideDefaults={true}
+      className="mx-auto w-full max-w-sm px-6 pt-0 pb-12 sm:max-w-xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl xl:px-0"
+    >
       {children}
     </Atoms.Container>
   );
