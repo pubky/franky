0598539b3817d6a22676c36a1b339460b9bef08c
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2025-11-19 12:02:10 +0100

    refactor: dont deprecate existing users stream prefix

--- src/core/services/nexus/stream/users/userStream.types.ts
+++ src/core/services/nexus/stream/users/userStream.types.ts
@@ -1,7 +1,8 @@
 import * as Core from '@/core';
 
 export enum USER_STREAM_PREFIX {
-  USERS = 'v0/stream/users/ids',
+  USERS = 'v0/stream/users',
+  USER_IDS = 'v0/stream/users/ids',
   USERNAME = 'v0/stream/users/username',
   USERS_BY_IDS = 'v0/stream/users/by_ids',
 }
