17a21809dc6901b540ab51ebf595138adecf1a30
James Browning <jamesbrowning91@gmail.com>
2025-12-03 11:40:18 +0000

    tests: consecutive sign up E2E

--- cypress/e2e/onboarding.cy.ts
+++ cypress/e2e/onboarding.cy.ts
@@ -10,16 +10,18 @@ describe('Onboarding', () => {
   beforeEach(() => {
     cy.deleteDownloadsFolder();
     cy.visit('/');
+    checkHeaderIsVisible();
   });
+
+  const checkHeaderIsVisible = () => {
+    cy.get('header').should('exist').should('be.visible');
+    cy.get('#header-sign-in-btn').should('exist').should('be.visible');
+  };
 
   it('can onboard as a new user backing up with encrypted file and recovery phrase, sign out, then sign in with both methods', () => {
     // profile name is also used for the backup file name
     const profileName = 'Test User';
     const backupWithFileAndPhrase = [BackupType.EncryptedFile, BackupType.RecoveryPhraseWithConfirmation];
-
-    // check header is visible
-    cy.get('header').should('exist').should('be.visible');
-    cy.get('#header-sign-in-btn').should('exist').should('be.visible');
 
     cy.onboardAsNewUser(profileName, 'Test Bio', backupWithFileAndPhrase);
 
@@ -48,5 +50,25 @@ describe('Onboarding', () => {
 
     cy.signOut(HasBackedUp.Yes);
     // todo: check users profile has correctly saved before sign out once implemented
+  });
+
+  // todo: reenable once bug is fixed, https://github.com/pubky/franky/issues/496
+  // fails at invite code stage due to last_read 401
+  it.skip('can sign up, sign out, then sign up again as a new user', () => {
+    const firstProfileName = 'First User';
+    const secondProfileName = 'Second User';
+
+    // sign up as first user
+    cy.onboardAsNewUser(firstProfileName);
+
+    // sign out
+    cy.signOut(HasBackedUp.Yes);
+
+    // verify we're back at the home page and can sign up again
+    cy.location('pathname').should('eq', '/');
+    checkHeaderIsVisible();
+
+    // sign up as second user
+    cy.onboardAsNewUser(secondProfileName);
   });
 });
