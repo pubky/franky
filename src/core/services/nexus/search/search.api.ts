106d1e97a9beafa3771e84b48ea009dc0037db7f
Kevin Karsopawiro <k.karsopawiro@gmail.com>
2025-12-04 11:46:28 +0100

    chore: cleanup

--- src/core/services/nexus/search/search.api.ts
+++ src/core/services/nexus/search/search.api.ts
@@ -8,6 +8,10 @@ import * as Core from '@/core';
 const PREFIX = 'v0/search';
 
 export const searchApi = {
+  byTag: (params: Core.TTagParams) => {
+    const tag = Core.encodePathSegment(params.tag);
+    return Core.buildUrlWithQuery(`${PREFIX}/posts/by_tag/${tag}`, params, Core.SEARCH_PATH_PARAMS);
+  },
   byPrefix: (params: Core.TPrefixSearchParams) => {
     const prefix = Core.encodePathSegment(params.prefix);
     return Core.buildUrlWithQuery(`${PREFIX}/tags/by_prefix/${prefix}`, params, Core.SEARCH_PATH_PARAMS);
