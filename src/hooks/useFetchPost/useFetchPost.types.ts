6ba4863837e98d0980fe26ae59afd82195d406cc
V <jovanovicv90@gmail.com>
2025-12-09 21:45:55 +0100

    Dead code deletion

--- src/hooks/useFetchPost/useFetchPost.types.ts
+++
@@ -1,6 +0,0 @@
-export interface UseFetchPostResult {
-  /** Function to fetch a post if it's missing */
-  fetchPost: (postId: string) => Promise<void>;
-  /** Whether a fetch is in progress */
-  isFetching: boolean;
-}
