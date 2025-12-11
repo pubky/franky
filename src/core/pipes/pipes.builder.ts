aaed18b8cf271723fd77d4ed9b962f2e5516d8de
James Browning <jamesbrowning91@gmail.com>
2025-12-11 16:49:27 +0000

    fix: reinitilise PubkySpecsBuilder on pubky change, bug #496

--- src/core/pipes/pipes.builder.ts
+++ src/core/pipes/pipes.builder.ts
@@ -3,12 +3,14 @@ import * as Core from '@/core';
 
 export class PubkySpecsSingleton {
   private static builder: PubkySpecsBuilder | null = null;
+  private static currentPubky: Core.Pubky | null = null;
 
   private constructor() {}
 
   static get(pubky: Core.Pubky): PubkySpecsBuilder {
-    if (!this.builder) {
+    if (!this.builder || this.currentPubky !== pubky) {
       this.builder = new PubkySpecsBuilder(pubky);
+      this.currentPubky = pubky;
     }
     return this.builder;
   }
