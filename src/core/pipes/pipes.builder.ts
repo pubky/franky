2f7fc54da3fae41fe12108739232dc94d33bf426
James Browning <jamesbrowning91@gmail.com>
2025-12-11 17:09:41 +0000

    fix: handle invalid pubky in PubkySpecsSingleton

--- src/core/pipes/pipes.builder.ts
+++ src/core/pipes/pipes.builder.ts
@@ -1,5 +1,6 @@
 import { PubkySpecsBuilder } from 'pubky-app-specs';
 import * as Core from '@/core';
+import * as Libs from '@/libs';
 
 export class PubkySpecsSingleton {
   private static builder: PubkySpecsBuilder | null = null;
@@ -8,9 +9,19 @@ export class PubkySpecsSingleton {
   private constructor() {}
 
   static get(pubky: Core.Pubky): PubkySpecsBuilder {
-    if (!this.builder || this.currentPubky !== pubky) {
+    if (!this.builder) {
       this.builder = new PubkySpecsBuilder(pubky);
       this.currentPubky = pubky;
+    }
+    // If pubky has changed, recreate the builder only if the new pubky is valid
+    else if (this.currentPubky !== pubky) {
+      try {
+        this.builder = new PubkySpecsBuilder(pubky);
+        this.currentPubky = pubky;
+      } catch {
+        Libs.Logger.error('Invalid pubky. Using existing builder.', { pubky });
+        return this.builder;
+      }
     }
     return this.builder;
   }
