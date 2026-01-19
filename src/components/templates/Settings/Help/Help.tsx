a2910ac5cb9cbac48beaf37df9b166973301abac
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2026-01-19 15:51:10 -0300

    refactor(SettingsSectionCard): enhance component structure and add wrapChildren prop
    - Updated SettingsSectionCard to include a new wrapChildren prop, allowing for direct rendering of children without the bordered container when set to false.
    - Adjusted the component's internal structure to conditionally render the header and description based on the presence of props.
    - Updated related tests and snapshots to reflect these changes.

--- src/components/templates/Settings/Help/Help.tsx
+++ src/components/templates/Settings/Help/Help.tsx
@@ -1,16 +1,11 @@
 'use client';
 
-import * as Atoms from '@/atoms';
 import * as Molecules from '@/molecules';
-import * as Libs from '@/libs';
 
 export function Help() {
   return (
-    <Atoms.Container
-      overrideDefaults
-      className={Libs.cn('flex flex-col items-start gap-6 rounded-md bg-card p-8 md:p-12')}
-    >
+    <Molecules.SettingsSectionCard wrapChildren={false}>
       <Molecules.HelpContent />
-    </Atoms.Container>
+    </Molecules.SettingsSectionCard>
   );
 }
